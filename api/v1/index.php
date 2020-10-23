4c1b540a14267ee1b6360e7d7e144f887daad05f
Obicrm <73349998+Obicrm@users.noreply.github.com>
2020-10-23 18:11:28 +0300

    Update index.php
    added Header

--- api/v1/index.php
+++ api/v1/index.php
@@ -6,5 +6,13 @@
 require_once('../../bootstrap.php');
 
 
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    // return only the headers and not the content
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Headers: X-Requested-With,Authorization,Espo-Authorization,Content-Type,Espo-Authorization-By-Token');
+    header('Access-Control-Allow-Methods: POST, GET, PUT, PATCH, DELETE, OPTIONS');
+    die;
+}
+
 $app = new \Espo\Core\Application();
 $app->run();
